3377e9e45e7d84c7915053b5a8cbef21f84310b4
Tu Nombre <tunombre@ejemplo.com>
2025-10-08 15:47:38 -0300

    actualizo deploy.yml

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+// service-worker.js
+const CACHE_NAME = "dashboard-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((c) => c.put(event.request, copy));
+          return res;
+        })
+      );
+    })
+  );
+});
